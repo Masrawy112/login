414d45db5566aae44dfd9de6805388a0dbf93457
Masrawy112 <elzrahmed@gmail.com>
2024-09-17 05:47:54 +0300

    Initial commit

---
+++ app/src/androidTest/java/com/example/second/ExampleInstrumentedTest.kt
@@ -0,0 +1,11 @@
+package com.example.second
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
